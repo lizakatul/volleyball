2f0414d2e891616ee4e4e541ccd988b6d6fd6e51
lizakatul <lizakatul@gmail.com>
2024-12-06 20:54:21 +0300

    improved version of hit detection

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
 pose = mp_pose.Pose()
 
-cap = cv2.VideoCapture('test/attack_test1.mov') #заменить на нужный файл
+cap = cv2.VideoCapture('test/attack_test6.mov')  # замените на нужный файл
 
-hit_display_time = 2
+hit_display_time = 5
 last_hit_time = 0
 last_hit_type = None
+hit_distance_threshold = 100
 
 def detect_hit(landmarks):
     if landmarks:
@@ -29,21 +30,24 @@ def detect_hit(landmarks):
         ear = landmarks[mp_pose.PoseLandmark.LEFT_EAR]
         hip_left = landmarks[mp_pose.PoseLandmark.LEFT_HIP]
         hip_right = landmarks[mp_pose.PoseLandmark.RIGHT_HIP]
+        elbow_left = landmarks[mp_pose.PoseLandmark.LEFT_ELBOW]
+        elbow_right = landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW]
 
         if wrist_left.y > shoulder_left.y and wrist_right.y > shoulder_right.y and wrist_left.y > hip_left.y and wrist_right.y > hip_right.y:
             return "Serve"
-        if abs(wrist_left.y - wrist_right.y) < 0.02 and abs(wrist_left.x - wrist_right.x) < 0.02:
+        if abs(wrist_left.y - wrist_right.y) < 0.1 and abs(wrist_left.x - wrist_right.x) < 0.08 and abs(elbow_right.y - wrist_right.y) < 0.05:
             return "Lower hit"
         elif (wrist_left.y < ear.y and wrist_right.y > shoulder_right.y) or (wrist_left.y > ear.y and wrist_right.y < shoulder_right.y):
             return 'Attack'
-        elif wrist_left.y < shoulder_left.y and wrist_right.y < shoulder_right.y:
+        elif elbow_left.y < shoulder_left.y and elbow_right.y < shoulder_right.y:
             return 'Upper hit'
+
     return None
 
 def distance(point1, point2):
     return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
 
-while cap.isOpened():
+while True:
     ret, frame = cap.read()
     if not ret:
         break
@@ -64,7 +68,7 @@ while cap.isOpened():
             cv2.circle(frame, ball_center, int(radius), (0, 255, 0), 2)
             cv2.circle(frame, ball_center, 5, (255, 0, 0), -1)
 
-    _, w, _ = frame.shape
+    h, w, _ = frame.shape
     person_1 = frame[:, :w // 2]
     person_2 = frame[:, w // 2:]
 
@@ -78,29 +82,45 @@ while cap.isOpened():
 
     closest_player = None
     closest_distance = float('inf')
+
     if ball_center:
         if landmarks_1:
-            wrist_left_1 = (int(landmarks_1[mp_pose.PoseLandmark.LEFT_WRIST].x * width // 2),int(landmarks_1[mp_pose.PoseLandmark.LEFT_WRIST].y * height))
-            wrist_right_1 = (int(landmarks_1[mp_pose.PoseLandmark.RIGHT_WRIST].x * width // 2),int(landmarks_1[mp_pose.PoseLandmark.RIGHT_WRIST].y * height))
-            distance_1 = min(distance(ball_center, wrist_left_1),distance(ball_center, wrist_right_1))
-            if distance_1 < closest_distance:
-                closest_distance = distance_1
-                closest_player = 1
+            wrist_left_1 = (int(landmarks_1[mp_pose.PoseLandmark.LEFT_WRIST].x * w // 2), int(landmarks_1[mp_pose.PoseLandmark.LEFT_WRIST].y * h))
+            wrist_right_1 = (int(landmarks_1[mp_pose.PoseLandmark.RIGHT_WRIST].x * w // 2), int(landmarks_1[mp_pose.PoseLandmark.RIGHT_WRIST].y * h))
+            distance_1 = min(distance(ball_center, wrist_left_1), distance(ball_center, wrist_right_1))
+        else:
+            distance_1 = float('inf')
 
         if landmarks_2:
-            wrist_left_2 = (int(landmarks_2[mp_pose.PoseLandmark.LEFT_WRIST].x * width // 2 + width // 2), int(landmarks_2[mp_pose.PoseLandmark.LEFT_WRIST].y * height))
-            wrist_right_2 = (int(landmarks_2[mp_pose.PoseLandmark.RIGHT_WRIST].x * width // 2 + width // 2), int(landmarks_2[mp_pose.PoseLandmark.RIGHT_WRIST].y * height))
+            wrist_left_2 = (int(landmarks_2[mp_pose.PoseLandmark.LEFT_WRIST].x * w // 2 + w // 2), int(landmarks_2[mp_pose.PoseLandmark.LEFT_WRIST].y * h))
+            wrist_right_2 = (int(landmarks_2[mp_pose.PoseLandmark.RIGHT_WRIST].x * w // 2 + w // 2), int(landmarks_2[mp_pose.PoseLandmark.RIGHT_WRIST].y * h))
             distance_2 = min(distance(ball_center, wrist_left_2), distance(ball_center, wrist_right_2))
-            if distance_2 < closest_distance:
+        else:
+            distance_2 = float('inf')
+
+        if landmarks_1 and landmarks_2:
+            if distance_1 < distance_2:
+                closest_distance = distance_1
+                closest_player = 1
+            else:
                 closest_distance = distance_2
                 closest_player = 2
+        elif landmarks_1:
+            closest_player = 1
+            closest_distance = distance_1
+        elif landmarks_2:
+            closest_player = 2
+            closest_distance = distance_2
 
-    if closest_player == 1 and landmarks_1:
-        hit_result = detect_hit(landmarks_1)
-    elif closest_player == 2 and landmarks_2:
-        hit_result = detect_hit(landmarks_2)
-    else:
-        hit_result = None
+    if closest_player is None:
+        closest_player = 1 if landmarks_1 else 2
+
+    hit_result = None
+    if closest_distance < hit_distance_threshold:
+        if closest_player == 1 and landmarks_1:
+            hit_result = detect_hit(landmarks_1)
+        elif closest_player == 2 and landmarks_2:
+            hit_result = detect_hit(landmarks_2)
 
     if hit_result:
         last_hit_time = time.time()
@@ -115,7 +135,7 @@ while cap.isOpened():
             serve_sound.play()
 
     if last_hit_type and (time.time() - last_hit_time < hit_display_time):
-        cv2.putText(frame, f"Player {closest_player}: {last_hit_type}", (20, 50), cv2.FONT_ITALIC, 1, (0, 255, 0), 2)
+        cv2.putText(frame, last_hit_type, (20, 50), cv2.FONT_ITALIC, 1, (0, 255, 0), 2)
 
     cv2.imshow('Hit Detection', frame)
 
@@ -124,5 +144,6 @@ while cap.isOpened():
 
 cap.release()
 cv2.destroyAllWindows()
+
 
 
